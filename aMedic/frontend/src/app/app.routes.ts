b8da79cd2a7d75730e99a1ef381ece750252d622
Vyshnav <vyshnavk78@gmail.com>
2024-10-09 21:28:50 +0530

    added home route

--- aMedic/frontend/src/app/app.routes.ts
+++ aMedic/frontend/src/app/app.routes.ts
@@ -1,7 +1,16 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component'
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from "./home/home.component";
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },    //default route
-    { path: '**', component: LoginComponent}    //any unknown route
+    { path: 'home', component: HomeComponent},
+
+
+
+
+
+
+
+    { path: '**', component: LoginComponent}    //any unknown route, should be at the last
 ];
