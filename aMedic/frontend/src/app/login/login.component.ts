764bb60f9c7c4de2910907009b9b93a9a10449dc
Vyshnav <vyshnavk78@gmail.com>
2024-10-09 17:09:44 +0530

    login inputs

--- aMedic/frontend/src/app/login/login.component.ts
+++ aMedic/frontend/src/app/login/login.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  // these 2 are properties bound to the input field uisng ngModel (this allows 2way data binding)
+  email: string='';
+  password: string='';
 
+  //  The constructor method is called when an instance of the component is created
+  constructor(){}
+
+  // functional logic 
+  onSubmit(){
+    console.log("email: ",this.email);
+    console.log("password: ",this.password);
+    
+  }
 }
