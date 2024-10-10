a4d0f3798a57b0607e57177bc162f1f21f013fa8
Vyshnav <vyshnavk78@gmail.com>
2024-10-10 23:31:45 +0530

    login form using angular/material io

--- aMedic/frontend/src/app/login/login.component.ts
+++ aMedic/frontend/src/app/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { Router } from "@angular/router";
-import { AuthService } from '../services/auth.service'; 
+import { AuthService } from '../services/auth.service';
+import { CommonModule } from '@angular/common';  
+
+
+// angular material.io modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,
+    CommonModule, 
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -14,6 +25,7 @@ export class LoginComponent {
   // these 2 are properties bound to the input field uisng ngModel (this allows 2way data binding)
   email: string='';
   password: string='';
+  loginError: string | null=null;
 
   //  The constructor method is called when an instance of the component is created
   constructor(private authService: AuthService, private router: Router){}   // Inject AuthService and Router
@@ -27,7 +39,7 @@ export class LoginComponent {
           this.router.navigate(['/home']);
         },
         error: (error) => {
-          alert("invalid creds");
+          this.loginError = "Invalid credentials"; // Update the error message
           console.error(error);
         },
         complete: () => {
