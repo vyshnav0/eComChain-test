9034fd20bb9f9bb254691fd907550445ef96fe6a
Vyshnav <vyshnavk78@gmail.com>
2024-10-10 12:31:35 +0530

    hashing and comparing password

--- aMedic/backend/routes/usersRoute.js
+++ aMedic/backend/routes/usersRoute.js
@@ -1,24 +1,22 @@
 const express = require('express')
 const User = require('../models/usersModel')
 const router = express.Router();
-
-// router.get('/', async (req, res)=>{
-//     try {
-//         const users = await User.find();
-//         res.status(200).json(users);
-//     } catch (error) {
-//         res.status(500).json({message: "Error getting users list", error: error});
-//     }
-// });
+const bcrypt = require('bcrypt');
 
 router.post('/login', async (req, res)=>{
     const {email, password} = req.body;
 
+    const mypass = "mypass";
+    const hashPass = await bcrypt.hash(mypass, 10);
+    console.log("pass: ",hashPass);
+    
     try {
         const user = await User.findOne({email});
         if(!user)
             return res.status(404).json({message: "User not found"});
-        if(password !== user.password)
+
+        const rightPass = await bcrypt.compare(password, user.password)
+        if(!rightPass)
             return res.status(400).json({message: "invalid password"})
 
         // if user is found and password is correct
