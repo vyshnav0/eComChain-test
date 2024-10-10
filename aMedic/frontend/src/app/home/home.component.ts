ef140a856f92173807a9786709e663be5ce1611b
Vyshnav <vyshnavk78@gmail.com>
2024-10-10 14:28:49 +0530

    products display service at home

--- aMedic/frontend/src/app/home/home.component.ts
+++ aMedic/frontend/src/app/home/home.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from "../services/product.service";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+// HomeComponent is using the OnInit lifecycle hook
+export class HomeComponent implements OnInit {
+  products: any[] = [];
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {  // the lifecycle hook mentioned above, runs when comp is initialised
+    this.productService.getProducts().subscribe({
+      next: (data) => {
+        // console.log("products",data);
+        this.products = data;
+      },
+      error: (err) => {
+        console.log("error fetching products from backend", err);
+      }
+    });
+  }
 }
