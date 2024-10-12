f7190d36caadcb380186790fb891e151663cbe08
Vyshnav <vyshnavk78@gmail.com>
2024-10-12 17:19:11 +0530

    authguard for logouts

--- aMedic/frontend/src/app/app.routes.ts
+++ aMedic/frontend/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from "./home/home.component";
 import { MedicineDetailsComponent } from './medicine-details/medicine-details.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },    //default route
-    { path: 'home', component: HomeComponent},
-    { path: 'meds/:id', component: MedicineDetailsComponent },
+    { path: 'home', component: HomeComponent, canActivate: [authGuard]},
+    { path: 'meds/:id', component: MedicineDetailsComponent, canActivate: [authGuard] },
 
 
 
---
+++ aMedic/frontend/src/app/auth.guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+
+  const router = inject(Router);
+
+  const token = localStorage.getItem('token');
+  if(!token){
+    router.navigate(['/'])
+    return false;
+  }
+  return true;
+};
+
+/* this authguard is used to prevent unauthorized access to pages. once you logged out, u cant access anything using the url
+*/
